451469c8d78b2ad902c7ede637d19ffb5bc3a3e0
JamieK <32877208+GitJamieK@users.noreply.github.com>
2025-01-14 13:11:25 +0100

    Added player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,27 +1,36 @@
-using Unity.Netcode;
 using UnityEngine;
+using Unity.Netcode;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : NetworkBehaviour
 {
-    public float speed = 5f;
-    public float jumpForce = 5f;
-    private Rigidbody2D rb;
+    public float moveSpeed = 5f;
 
-    private void Start()
+    private PlayerControls playercontrols;
+    private Vector2 moveInput;
+
+    private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
+        playercontrols = new PlayerControls();
+    }
+
+    private void OnEnable()
+    {
+        playercontrols.Enable();
+        playercontrols.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        playercontrols.Player.Move.canceled += ctx => moveInput = Vector2.zero;
+    }
+
+    private void OnDisable()
+    {
+        playercontrols.Disable();
     }
 
     private void Update()
     {
         if (!IsOwner) return;
-
-        float move = Input.GetAxis("Horizontal");
-        rb.linearVelocity = new Vector2(move * speed, rb.linearVelocity.y);
-
-        if (Input.GetButtonDown("Jump") && Mathf.Abs(rb.linearVelocity.y) < 0.01f)
-        {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-        }
+        
+        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y) * moveSpeed * Time.deltaTime;
+        transform.Translate(move, Space.World);
     }
 }
